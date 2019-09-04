f1f1125e79c370413768c2b3bb07b915af748633
YuanS1119 <1650216381@qq.com>
2019-09-04 23:47:12 +0800

    汉化

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import './style/index.less'
-import VeeValidate from 'vee-validate'
+import VeeValidate, {Validator} from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 
 Vue.use(Vant)
 Vue.use(VeeValidate)
+Validator.localize('zhCN',zhCN)
 Vue.config.productionTip = false
 // const vm =
 new Vue({
