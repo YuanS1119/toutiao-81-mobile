1ecd403b36bb81b1163509d28e990b3df43a1be6
YuanS1119 <1650216381@qq.com>
2019-09-05 11:57:56 +0800

    tabbar的组件

--- src/views/Tabber.vue
+++ src/views/Tabber.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-      <router-view></router-view>
-      tabber
+    <router-view></router-view>
+    <van-tabbar>
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="ask" icon="search">问答</van-tabbar-item>
+      <van-tabbar-item to="video" icon="friends-o">视频</van-tabbar-item>
+      <van-tabbar-item to="self" icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'tabbar'
 }
 </script>
 
 <style>
-
 </style>
