996de9a23e51ce851609ccd639f4e98797c07258
YuanS1119 <1650216381@qq.com>
2019-09-05 19:47:52 +0800

    首页简单布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-      <router-view></router-view>
-      home
+    <router-view></router-view>
+    <van-nav-bar title="黑马头条" left-arrow />
+    <van-tabs animated>
+      <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">内容 {{ index }}</van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      active: ''
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
