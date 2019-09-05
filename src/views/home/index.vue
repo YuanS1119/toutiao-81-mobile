4e00e261646124b8eb5fd0762219a8a01cf4bc82
YuanS1119 <1650216381@qq.com>
2019-09-05 20:37:22 +0800

    list组件和深度选择器

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <router-view></router-view>
-    <van-nav-bar title="黑马头条" left-arrow />
+    <van-nav-bar title="黑马头条" left-arrow fixed />
     <van-tabs animated>
-      <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">内容 {{ index }}</van-tab>
+      <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+          <van-cell v-for="item in list" :key="item" :title="item" />
+        </van-list>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
@@ -12,11 +16,34 @@
 export default {
   data () {
     return {
-      active: ''
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
     }
   }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+  .van-tabs /deep/ .van-tabs__content {
+    margin: 46px 0 50px;
+  }
 </style>
