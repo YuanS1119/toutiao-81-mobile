790268e94b3737b0aa2c94448a69f958cf50e74e
YuanS1119 <1650216381@qq.com>
2019-09-08 19:25:06 +0800

    弹层内容显示并控制弹层的显示隐藏

--- src/components/tipSwinDown.vue
+++ src/components/tipSwinDown.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-      1323
-  </div>
+  <van-dialog v-model="show" :showConfirmButton="false" closeOnClickOverlay >
+    <!-- 弹框提示信息 -->
+    <van-cell-group v-show="!showReport">
+      <van-cell icon="location-o" title="不感兴趣" />
+      <van-cell icon="location-o" title="反馈垃圾内容" is-link @click="showReport = true"/>
+      <van-cell icon="location-o" title="拉黑作者" />
+    </van-cell-group>
+    <!-- 举报弹框 -->
+   <van-cell-group v-show="showReport">
+      <van-cell icon="arrow-left" @click="showReport = false"/>
+      <van-cell title="标题夸张" />
+      <van-cell title="低俗色情" />
+      <van-cell title="错别字多" />
+      <van-cell title="旧闻重复" />
+      <van-cell title="广告软文" />
+      <van-cell title="内容不实" />
+      <van-cell title="涉嫌违法犯罪" />
+      <van-cell title="侵权" />
+      <van-cell title="其他问题" />
+    </van-cell-group>
+  </van-dialog>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      show: true,
+      showReport: false
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
