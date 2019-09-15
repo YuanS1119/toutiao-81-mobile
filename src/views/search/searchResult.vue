ddf1c9229d0b143f070228525b1cf4dd713b7cd6
YuanS1119 <1650216381@qq.com>
2019-09-15 21:37:23 +0800

    搜索-搜索结果图文展示

--- src/views/search/searchResult.vue
+++ src/views/search/searchResult.vue
@@ -3,7 +3,34 @@
     <van-nav-bar title="搜索结果" left-text="返回" left-arrow @click-left="$router.back()" />
     <!-- 搭建搜索列表组件 -->
     <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <van-cell v-for="item in searchList" :key="item.art_id.toString()" :title="item.title" />
+      <van-cell
+        v-for="article in searchList"
+        :key="article.art_id.toString()"
+        :title="article.title"
+      >
+        <!-- 插入图片（使用cell中的插槽） -->
+        <div slot="label">
+          <!-- type封面类型，0-无封面，1-1张封面图片，3-3张封面 -->
+          <van-grid :column-num="3" v-if="article.cover.type" :border="false">
+            <van-grid-item v-for="(img,index) in article.cover.images" :key="img + index">
+              <van-image height="80" :src="img" lazy-load>
+                <!-- lazy-load图片懒加载 -->
+                <!-- 图片加载提示 -->
+                <template v-slot:loading>
+                  <van-loading type="spinner" size="20" />
+                </template>
+                <!-- 加载失败提示 -->
+                <template v-slot:error>加载失败</template>
+              </van-image>
+            </van-grid-item>
+          </van-grid>
+          <p>
+            <span>{{article.aut_name}}</span>&nbsp;
+            <span>{{article.comm_count}}评论</span>&nbsp;
+            <span>{{article.pubdate | fmtDate}}</span>
+          </p>
+        </div>
+      </van-cell>
     </van-list>
   </div>
 </template>
@@ -20,7 +47,7 @@ export default {
       finished: false,
       // 页码参数
       page: 1, // 页数默认不传为1
-      per_page: 10// 每页多少条，不传有后端决定
+      per_page: 10 // 每页多少条，不传有后端决定
     }
   },
   methods: {
@@ -43,30 +70,14 @@ export default {
       } catch (error) {
         this.$toast.fail('获取搜索结果失败')
       }
-      // // 异步更新数据
-      // setTimeout(() => {
-      //   for (let i = 0; i < 10; i++) {
-      //     this.searchList.push(this.searchList.length + 1)
-      //   }
-      //   // 加载状态结束
-      //   this.loading = false
-
-      //   // 数据全部加载完成
-      //   if (this.searchList.length >= 40) {
-      //     this.finished = true
-      //   }
-      // }, 500)
     }
-
   },
-  created () {
-
-  }
+  created () {}
 }
 </script>
 
 <style lang="less" scoped>
 .van-list {
-  margin-top: 46px;
+  // margin-top: 46px;
 }
 </style>
